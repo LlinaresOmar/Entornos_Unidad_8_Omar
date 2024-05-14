bac5d66c8a3aaf0f80807fe762f598eaca97482d
Omar <llinares.omar@gmail.com>
2024-05-14 13:14:03 +0200

    3º Commit Anys
    -Comentada la clase

--- Tasca1/Algoritmes/src/es/ies/mz/ed/algoritmes/AnyRepetits.java
+++ Tasca1/Algoritmes/src/es/ies/mz/ed/algoritmes/AnyRepetits.java
@@ -3,9 +3,24 @@ package es.ies.mz.ed.algoritmes;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que contiene el metodo para averiguar el numero de digitos de un anyo.
+ */
+
 public class AnyRepetits {
 
+    /**
+     * Constructor vacio
+     */
     public AnyRepetits(){}
+
+    /**
+     * Metodo que recibe un anyo en long y lo convierte en String.
+     * Crea un arrayList vacio de caracteres.
+     * Recorre los caracteres del numero y si no existe en el arraylist lo añade.
+     * @param any anyo a tratar para obtener el numero de caracteres repetidos.
+     * @return el numero de los diferntes caracteres que componen el anyo.
+     */
 
     public int numRepetits(long any){
        int numRepetidos;
